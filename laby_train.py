7f9debb29658600793c5974f335ebbdbd8581c20
Brice Hoareau <bricesh@yahoo.com>
2018-01-18 18:08:22 +0100

    Update laby_train.py
    Added previous position features

--- laby_train.py
+++ laby_train.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
 import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 
 # Load file
-with open('labys/saved_games_small.txt', 'r') as f:
+with open('labys/saved_games_small_rdm.txt', 'r') as f:
     paths = [line.replace('\n', '').split(',') for line in f]
 
 num_list = []
@@ -18,16 +22,16 @@ num_array = np.array(num_list).reshape(len(num_list)/7,7)
 training_data = pd.DataFrame(num_array, columns=["x", "y", "up", "down", "left", "right", "direction"])
 training_action = training_data[["direction"]]
 training_features = training_data[["x", "y", "up", "down", "left", "right"]]
-training_features["direction"].value_counts() # Could improve the class balance...
+training_features["pre_x"] = training_features["x"].shift(1)
+training_features["pre_y"] = training_features["y"].shift(1)
+training_features.loc[training_features["x"]==0,"pre_x"]=training_features[training_features["x"]==0]["x"]
+training_features.loc[training_features["x"]==0,"pre_y"]=training_features[training_features["x"]==0]["y"]
 
-from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
+training_action["direction"].value_counts() # Could improve the class balance...
 
 train_x, test_x, train_y, test_y = train_test_split(training_features,
                                                     training_action,
-                                                    train_size=.6)
+                                                    train_size=.4)
 
 rf_clf = RandomForestClassifier()
 rf_clf.fit(train_x, train_y)
@@ -39,4 +43,4 @@ print "Test Accuracy  :: ", accuracy_score(test_y, predictions)
 print " Confusion matrix "
 print confusion_matrix(test_y, predictions)
 
-pickle.dump(rf_clf, open( "rf_clf.p", "wb" ))
+pickle.dump(rf_clf, open( "rf_clf_with_prexy.p", "wb" ))
